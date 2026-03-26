6fd8365020c61e2267a6f6538fafd69725133199
Ariful Hasan <arifhasan.mbstu@gmail.com>
2026-03-26 23:53:41 +0100

    finally update routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\WalletController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post  ('wallets',                    [WalletController::class, 'open']);
+    Route::get   ('wallets/{walletId}',         [WalletController::class, 'show']);
+    Route::post  ('wallets/{walletId}/deposit', [WalletController::class, 'deposit']);
+    Route::post  ('wallets/{walletId}/transfer',[WalletController::class, 'transfer']);
 });
